a5698308a7693ac7d9f565c394ece9adb8c8463c
yinghui725 <yinghui725@vip.sina.com>
2025-05-09 14:38:27 +0800

    修改中继服务器域名和key

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rccd.gzjingk.com"];
+pub const RS_PUB_KEY: &str = "NKsUTW0LxIR0mvW1Fdlbk53RF8F8NHUgut6W2n6REIg=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
